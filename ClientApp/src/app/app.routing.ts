1e46cbf0c7e542d621619068cb1e04903d601318
fabiossussin <fabio@xplay.digital>
2022-10-05 13:50:54 -0300

    Implementado cadastro e listagem fe empresas

--- ClientApp/src/app/app.routing.ts
+++ ClientApp/src/app/app.routing.ts
@@ -4,10 +4,11 @@ import { AuthGuard } from './core/guards/auth-guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)},
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule), canActivate: [AuthGuard] },
   { path: 'users', loadChildren: () => import('./user/user.module').then(m => m.UserPageModule), canActivate: [AuthGuard] },
-  { path: 'employee', loadChildren: () => import('./employees/employee.module').then(m => m.EmployeePageModule), canActivate: [AuthGuard] }
+  { path: 'employee', loadChildren: () => import('./employees/employee.module').then(m => m.EmployeePageModule), canActivate: [AuthGuard] },
+  { path: 'companies', loadChildren: () => import('./companies/company.module').then(m => m.CompanyPageModule), canActivate: [AuthGuard] }
 ];
 
 @NgModule({
