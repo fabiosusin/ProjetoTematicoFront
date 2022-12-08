c4df038a208cbeb827be1af5c2982d269b9dbafd
fabiosusin <fabiosusinoberherr@gmail.com>
2022-12-08 02:26:52 -0300

    finalizado migração para electron

--- ClientApp/src/environments/environment.prod.sagu.ts
+++
@@ -1,8 +0,0 @@
-import { Environment } from 'src/app/core/models/enum/environment.enum';
-
-export const environment = {
-  production: true,
-  name: Environment.prod,
-  apiBaseUrl: 'https://xapi-sagu.intra.digital:443/v1/',
-  apiUrl: 'https://xapi-sagu.intra.digital:443/v1/Intra/'
-};
--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { Environment } from 'src/app/core/models/enum/environment.enum';
 export const environment = {
   production: true,
   name: Environment.prod,
-  apiBaseUrl: 'https://xapi.intra.digital:443/v1/',
-  apiUrl: 'https://xapi.intra.digital:443/v1/Intra/'
+  apiBaseUrl: 'http://localhost:44363/v1/',
+  apiUrl: 'http://localhost:44363/v1/Intra/'
 };
